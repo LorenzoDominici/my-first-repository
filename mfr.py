085b3f7e934386b6002b22df48b001168ce48c8c
Dommo <lorenzo.dominici@student.ie.edu>
2021-10-07 14:44:54 +0200

    create the mfr.py

---
+++ mfr.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  7 14:12:08 2021
+
+@author: Dommo
+"""
+
+def func (a, b,):
+    return a + b
+
+def func2 (a,b,):
+    return a * b 
+ 
